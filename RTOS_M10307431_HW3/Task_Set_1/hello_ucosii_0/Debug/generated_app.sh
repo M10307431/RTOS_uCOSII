8664d8f7698abf8df728ec2c7bd010cf259c6792
M10307431 <chiang404@gmail.com>
2016-07-06 11:15:58 +0800

    aperiodic task - CUS

---
+++ RTOS_M10307431_HW3/Task_Set_1/hello_ucosii_0/Debug/generated_app.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+ 
+# DO NOT MODIFY THIS FILE
+#
+#   Changing this file will have subtle consequences
+#   which will almost certainly lead to a nonfunctioning
+#   system. If you do modify this file, be aware that your
+#   changes will be overwritten and lost when this file
+#   is generated again.
+#
+# DO NOT MODIFY THIS FILE
+ 
+SYSTEM_CONFIG_DIR=/cygdrive/e/Altera/80/kits/cycloneIII_3c120_dev/examples/cycloneIII_3c120_dev_niosII_standard/software/hello_ucosii_0_syslib/Debug
